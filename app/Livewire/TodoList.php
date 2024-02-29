7e2b5f9a2e57d751974ffd70e6792f141d6f129f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-29 17:50:19 +0200

    Show Todos

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -26,6 +26,8 @@ class TodoList extends Component
 
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', [
+            'todos' => Todo::latest()->get(),
+        ]);
     }
 }
