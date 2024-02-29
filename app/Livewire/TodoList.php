6d03820a13782d2ce3306925a5350b241e0eaf9b
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-29 18:34:55 +0200

    Delete Todo

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -27,6 +27,11 @@ class TodoList extends Component
         session()->flash('success', 'Task Created');
     }
 
+    public function delete(Todo $todo)
+    {
+        $todo->delete();
+    }
+
     public function render()
     {
         return view('livewire.todo-list', [
